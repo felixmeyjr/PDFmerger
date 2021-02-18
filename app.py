abd222cbaed960f50f4ad636b0d8ca4998d0fe57
Felix <felix.meyer97@web.de>
2021-02-18 21:23:06 +0100

    change the input parameters for the merger function

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import PyPDF2
 
 app = Flask(__name__)
 
-def merger(input_paths):
+# Merge the pdf, which are uploaded on the website
+def merger(input_files, input_path):
     pdf_merger = PyPDF2.PdfFileMerger()
-    for i in input_paths:
+    for i in input_files:
         pdf_merger.append(i)
-    output_path = input_paths[0]
+    output_path = input_path
     with open(output_path, "wb") as f_out:
         pdf_merger.write(f_out)
+    return f_out
 
 @app.route('/')
 def index():

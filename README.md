3d8ccaa41c63ec62ddb34b55752dcad952422f97
Felix <felix.meyer97@web.de>
2021-02-19 19:15:25 +0100

    created a sql, TODO: get the files into the db and merge them, create an upload afterwards

---
+++ README.md
@@ -0,0 +1,6 @@
+### Simple PDF Merger
+
+## TODO
+- Implement database
+- Return file as download after merging
+- Find a more simple way to parse input pdfs
--- app.py
+++ app.py
@@ -1,22 +1,73 @@
-from flask import Flask, render_template
-import PyPDF2
+from flask import Flask, render_template, request, make_response, send_file
+from PyPDF2 import PdfFileMerger
+import sqlite3 as sql
 
 app = Flask(__name__)
 
-# Merge the pdf, which are uploaded on the website
-def merger(input_files, input_path):
-    pdf_merger = PyPDF2.PdfFileMerger()
-    for i in input_files:
-        pdf_merger.append(i)
-    output_path = input_path
-    with open(output_path, "wb") as f_out:
-        pdf_merger.write(f_out)
-    return f_out
 
-@app.route('/')
+def createDB():
+    conn = sql.connect('database.db')
+    app.logger.info("Opened database successfully")
+    conn.execute("CREATE TABLE files (filename TEXT, file BLOB)")
+    conn.close()
+
+
+@app.route('/', methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    response = make_response(render_template("index.html", title="home"))
+    return response
+
+
+# Upload the files to a sql database
+@app.route("/upload/", methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        try:
+            f = request.files.getlist("mergeFile[]")
+            currentFiles = []
+
+            con = sql.connect("database.db")
+
+            cur = con.cursor()
+
+            print(f.filename)
+            print(con)
+            print(cur)
+            print("file:" + f.filename)
+
+            cur.execute("INSERT INTO files (filename,file) VALUES (?,?)", (f.filename, f.read()))
+            con.commit()
+            print("Record successfully added")
+
+            cur.close()
+            con.close()
+        except:
+            print("error in insert operation")
+            return render_template('index.html', file_names=currentFiles, title='Home')
+        finally:
+            return render_template('index.html', file_names=currentFiles, title='Home')
+
+
+# Get the files from database and merge
+@app.route("/merge/", methods=['POST'])
+def merger():
+    pdf_merger = PdfFileMerger()
+    con = sql.connect("database.db")
+    cursor = con.cursor()
+    input_files = cursor.execute("SELECT * FROM files")
+
+    for row in input_files:
+        pdf_merger.append(row)
+        # merger.append(io.BytesIO(row[3]))
+
+    with open('./downloads/result.pdf', "wb") as f_out:
+        pdf_merger.write(f_out)
+    return send_file('./downloads/result.pdf',
+                     mimetype='application/pdf',
+                     attachment_filename='result.pdf',
+                     as_attachment=True)
 
 
 if __name__ == '__main__':
-    app.run()
+    # createDB()
+    app.run(debug=True)

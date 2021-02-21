0a3e5a1d7c6b6f051fea22972d0158725690d606
Felix <felix.meyer97@web.de>
2021-02-21 01:14:43 +0100

    complete rework, back to the basics rebase

--- app.py
+++ app.py
@@ -1,73 +1,109 @@
-from flask import Flask, render_template, request, make_response, send_file
+from flask import Flask
+
+from flask import redirect, url_for, request, render_template, send_file, flash
+from io import BytesIO
+
+from flask_wtf.file import FileField
+from wtforms import SubmitField
+from flask_wtf import FlaskForm
+
 from PyPDF2 import PdfFileMerger
 import sqlite3 as sql
+import io
+import sys
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret" # is necessary for Flask wtf
 
 
-def createDB():
-    conn = sql.connect('database.db')
-    app.logger.info("Opened database successfully")
-    conn.execute("CREATE TABLE files (filename TEXT, file BLOB)")
+# class UploadFile(FlaskForm):
+#     file = FileField()
+#     submit = SubmitField("Submit!")
+#     download = SubmitField("Download!")
+
+
+# A simple sql database
+def database_in(name, data):
+    conn = sql.connect("database.db")
+    cursor = conn.cursor()
+
+
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS new_table (name TEXT, data BLOB)")  # BLOB: Binary large object
+
+    # Delete all existing items first
+    cursor.execute("DELETE FROM new_table")
+
+    # Add new items back up
+    cursor.execute("INSERT INTO new_table (name, data) VALUES (?,?)", (name, data))
+
+    conn.commit()
+    cursor.close()
     conn.close()
 
-
-@app.route('/', methods=["POST", "GET"])
+#
+@app.route('/', methods=["POST", "GET"]) # necessary for the url_for part in html file
 def index():
-    response = make_response(render_template("index.html", title="home"))
-    return response
+    # form = UploadFile()
+
+    if request.method == "POST":
+        # if 'files[]' not in request.files:
+
+        files = request.files.getlist("files[]")
+        print("Found files!", file=sys.stderr)
+
+        output_files = merger(files)
+
+        # for file in files:
+        #     database_in(name=file.filename, data=file.read())
+        #     print(f"Successfully added: {file} to the database", file=sys.stderr)
+
+        # return render_template("index.html", form=form)
+        return render_template("index.html")
+    else:
+        # return render_template("index.html", form=form)
+        return render_template("index.html")
 
 
-# Upload the files to a sql database
-@app.route("/upload/", methods=['POST'])
-def upload():
-    if request.method == 'POST':
-        try:
-            f = request.files.getlist("mergeFile[]")
-            currentFiles = []
-
-            con = sql.connect("database.db")
-
-            cur = con.cursor()
-
-            print(f.filename)
-            print(con)
-            print(cur)
-            print("file:" + f.filename)
-
-            cur.execute("INSERT INTO files (filename,file) VALUES (?,?)", (f.filename, f.read()))
-            con.commit()
-            print("Record successfully added")
-
-            cur.close()
-            con.close()
-        except:
-            print("error in insert operation")
-            return render_template('index.html', file_names=currentFiles, title='Home')
-        finally:
-            return render_template('index.html', file_names=currentFiles, title='Home')
+# @app.route('/download_merged_file', methods=["POST", "GET"])
+# def download_merged_file():
+#
+#     print("Onto the merging", file=sys.stderr)
+#
+#     if request.method == "POST":
+#
+#         # Get the two files
+#         conn = sql.connect("database.db")
+#         cursor = conn.cursor()
+#         cursor.execute("SELECT * FROM new_table")
+#         input_files = list(cursor.fetchall())
+#
+#         conn.commit()
+#         cursor.close()
+#         conn.close()
+#
+#         # Call the function and parse input_files
+#         output_file = merger(input_files)
+#
+#         # Put merged file to a download
+#         return send_file(BytesIO(output_file), attachment_filename='merged.pdf', as_attachment=True)
+#
+#     return render_template("index.html")
 
 
 # Get the files from database and merge
-@app.route("/merge/", methods=['POST'])
-def merger():
+def merger(input_files):
     pdf_merger = PdfFileMerger()
-    con = sql.connect("database.db")
-    cursor = con.cursor()
-    input_files = cursor.execute("SELECT * FROM files")
 
-    for row in input_files:
-        pdf_merger.append(row)
-        # merger.append(io.BytesIO(row[3]))
+    for file in input_files:
+        print(f"File {file}")
+        # pdf_merger.append(io.BytesIO(file[3]))
 
     with open('./downloads/result.pdf', "wb") as f_out:
         pdf_merger.write(f_out)
-    return send_file('./downloads/result.pdf',
-                     mimetype='application/pdf',
-                     attachment_filename='result.pdf',
-                     as_attachment=True)
+
+    return f_out
 
 
 if __name__ == '__main__':
-    # createDB()
-    app.run(debug=True)
+    app.run()
